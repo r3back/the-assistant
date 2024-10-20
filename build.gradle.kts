d243d0f4e50973acd8f8901f8b632d03a4fbf0a6
r3back <andresponce1334@gmail.com>
2024-10-20 17:48:56 -0300

    Updated version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.github.r3back"
-version = "3.2.12"
+version = "3.2.13"
 
 ext.set("mojangUnmappedVersions", setOf("v1_20_R3","v1_20_R2", "v1_20_R1", "v1_19_R1", "v1_19_R2", "v1_18_R1", "v1_18_R2", "v1_17_R1"))
 
